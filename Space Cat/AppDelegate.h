6b0eecb5f2482c1884d147954ff88d9e9acf3b19
Ed McCormic <edmccormic@gmail.com>
2015-06-12 14:26:11 -0600

    Initial Commit

---
+++ Space Cat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Space Cat
+//
+//  Created by ED on 6/12/15.
+//  Copyright (c) 2015 SwiftBeard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
